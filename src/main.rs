e7845faf25399e606b5e966018b7d67d5909974d
AC Nine <bhekainnocentn@gmail.com>
2026-04-09 12:34:23 +0200

    TUI

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Read};
+use std::{fs::File, io::Read, process::Command};
 
 use clap::Parser;
 use ratatui::{
@@ -31,8 +31,9 @@ impl State {
 
 fn main() -> color_eyre::Result<()> {
     let arg = CLI::parse();
+    let path = arg.path.clone();
     let mut state = State { offset: 0 };
-    let mut file: File = match File::open(arg.path) {
+    let mut file: File = match File::open(path) {
         Ok(file) => file,
         Err(error) => {
             panic!("File not found! {}", error);
@@ -69,6 +70,10 @@ fn main() -> color_eyre::Result<()> {
                             state.up();
                         }
                     }
+                    KeyCode::Char('E') => {
+                        let path = arg.path.clone();
+                        bash(&path);
+                    }
                     _ => {}
                 }
             }
@@ -94,22 +99,42 @@ fn render(frame: &mut Frame, contents: &mut String, offset: usize) {
             );
         } else if line[count].starts_with("##") {
             frame.render_widget(
-                Paragraph::new(line[count].replacen("##", "", 1))
-                    .bold()
-                    .centered(),
+                Paragraph::new(line[count].replacen("##", "", 1)).bold(),
                 split[count - offset],
             );
         } else if line[count].starts_with("#") {
             frame.render_widget(
                 Paragraph::new(line[count].replacen("#", "", 1))
                     .bold()
-                    .underlined()
                     .centered(),
+                split[count - offset],
+            );
+        } else if line[count].starts_with("*") {
+            frame.render_widget(
+                Paragraph::new(line[count].replacen("*", "    •", 1)),
                 split[count - offset],
             );
         } else {
             frame.render_widget(Paragraph::new(line[count]), split[count - offset]);
         }
         count += 1;
+    }
+}
+
+fn bash(path: &str) {
+    let status = Command::new("bash")
+        .arg("-c")
+        .arg(
+            r#"
+            LOCATION="$1"
+            nvim "$LOCATION"
+        "#,
+        )
+        .arg("bash")
+        .arg(path)
+        .status();
+
+    if let Err(e) = status {
+        eprintln!("Failed to execute process: {}", e);
     }
 }
